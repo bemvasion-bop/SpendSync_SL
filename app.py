72d882cb0d86ca3efd26dac6028b2c2afe718b11
bemvasinbop <latikon43@gmail.com>
2026-05-14 12:07:13 -0700

    Debug Supabase insert

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from supabase import create_client
 import joblib
 import numpy as np
 
@@ -7,6 +8,12 @@ app = Flask(__name__)
 # Load model and scaler
 model = joblib.load('spendsync_model.pkl')
 scaler = joblib.load('scaler.pkl')
+
+# Supabase connection
+SUPABASE_URL = "https://obmulfyyzyubkpvnoimz.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ibXVsZnl5enl1Ymtwdm5vaW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MDQyNjAsImV4cCI6MjA5NDI4MDI2MH0.rGFkTmrZXTxmaV8oylvoJdSEChsx0gKLg3C3kmStbkM"
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route('/')
 def home():
@@ -30,13 +37,15 @@ def predict():
 
     # Arrange features
     features = np.array([[
+
         amount,
-        days,
         prev_claims,
         freq,
+        days,
         receipt,
         high_expense,
         frequent
+
     ]])
 
     # Scale numerical columns
@@ -48,7 +57,25 @@ def predict():
     prediction = model.predict(features)[0]
 
     # Convert prediction to label
-    result = "REJECTED" if prediction == 1 else "APPROVED"
+    result = "APPROVED" if prediction == 1 else "REJECTED"
+
+    # Convert receipt text
+    receipt_text = "Yes" if receipt == 1 else "No"
+
+
+    print("INSERTING INTO SUPABASE...")
+
+    # Save to Supabase
+    supabase.table("prediction_logs").insert({
+
+        "expense_amount": amount,
+        "days_after_expense": days,
+        "previous_claims": prev_claims,
+        "claim_frequency": freq,
+        "receipt_attached": receipt_text,
+        "prediction_result": result
+
+    }).execute()
 
     return jsonify({
         'result': result
